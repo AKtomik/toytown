c4b07a6ca99e568ed3b7dc6eda617467543758af
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-12-03 16:52:13 +0100

    settings first use

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -22,18 +22,21 @@ public class UnitTask {
 
     public void UpdateRepercution(Unit unit, double delta)
 	{
-		double factor = delta / 60;
+		double factor = delta / Settings.DayLengthInSecond;
 		unit.energyScore += this.energyPerDay * factor;
 		unit.saturationScore += this.saturationPerDay * factor;
 		unit.happynessScore += this.happynessPerDay * factor;
 	}
 
     public static UnitTask WANDERING = new(-.2, -.2, 0);
-    public static UnitTask WORKING = new(-.5, -.5, 0);
+    public static UnitTask WORKING = new(-.5, -.5, .1);
     public static UnitTask LEARNING = new(-.5, -.3, 0);
-    //public static UnitTaskRepercution WAITING = new(-.2, -.2, 0);
-    //public static UnitTaskRepercution WALKING = new(-.2, -.2, 0);
-    //public static UnitTaskRepercution EATING = new(-.2, -.2, 0);
+    //public static UnitTask WAITING = new(-.2, -.2, 0);
+    //public static UnitTask WALKING = new(-.2, -.2, 0);
+
+    // temporary
+    public static UnitTask EATING = new(-.1, 10, 0);
+    public static UnitTask SLEEPING = new(10, -.1, 0);
 }
 
 
@@ -42,8 +45,8 @@ public class Unit : MonoBehaviour
     public double saturationScore = 1;
     public double energyScore = 1;
     public double happynessScore = .5;
-    public UnitJob actualJob = UnitJob.NOTHING;
     public UnitTask actualTask = UnitTask.WANDERING;
+    public UnitJob actualJob = UnitJob.NOTHING;
         
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
