8acf5301e622ac1375beccb868292f0cd9cae402
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-12-03 15:58:48 +0100

    unit scores and job attributs

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,7 +1,21 @@
 using UnityEngine;
 
+public enum UnitJob {
+    NOTHING,
+    FARMER,
+    LUMBERJACK,
+    MINER,
+    BUILDER,
+}
+
+
 public class Unit : MonoBehaviour
 {
+    public double saturationScore = 1;
+    public double energyScore = 1;
+    public double happynessScore = .5;
+    public UnitJob actualJob = UnitJob.NOTHING;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
