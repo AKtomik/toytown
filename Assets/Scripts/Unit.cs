a2d4e1d8984926f630981f6e4f8ec2e03e53ea44
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-12-03 16:17:56 +0100

    Unit Task

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -8,6 +8,26 @@ public enum UnitJob {
     BUILDER,
 }
 
+public class UnitTask {
+    double energyPerDay;
+    double saturationPerDay;
+    double happynessPerDay;
+
+    private UnitTask(double energyPerDay, double saturationPerDay, double happynessPerDay)
+	{
+		this.energyPerDay = energyPerDay;
+		this.saturationPerDay = saturationPerDay;
+		this.happynessPerDay = happynessPerDay;
+	}
+
+    public static UnitTask WANDERING = new(-.2, -.2, 0);
+    public static UnitTask WORKING = new(-.5, -.5, 0);
+    public static UnitTask LEARNING = new(-.5, -.3, 0);
+    //public static UnitTaskRepercution WAITING = new(-.2, -.2, 0);
+    //public static UnitTaskRepercution WALKING = new(-.2, -.2, 0);
+    //public static UnitTaskRepercution EATING = new(-.2, -.2, 0);
+}
+
 
 public class Unit : MonoBehaviour
 {
@@ -15,7 +35,8 @@ public class Unit : MonoBehaviour
     public double energyScore = 1;
     public double happynessScore = .5;
     public UnitJob actualJob = UnitJob.NOTHING;
-    
+    public UnitJob actualTask = UnitJob.NOTHING;
+        
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
