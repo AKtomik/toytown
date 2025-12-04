27ff1950d48039819ef7a25acb3a64fe07c72ba4
Pablo-Thieriot <pablo.thieriot@gmail.com>
2025-12-04 14:00:33 +0100

    debut de code

---
+++ Assets/Scripts/Map/Tile.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    private bool isBuildable;
+
+    /*
+    --1 =  plaines
+    --2 = rock
+    --3 = food
+    --4 = wood
+    */
+    [SerializeField]
+    private int typeRessources;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
