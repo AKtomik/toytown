3f7ab857d59b7dab81e9b46eec79690f9d5920e7
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-12-04 14:24:43 +0100

    fix namespace error & tabs are better

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,60 +5,60 @@ using System.Collections.Generic;
 
 namespace ToyTown
 {
-    public enum UnitJob {
-    	NOTHING,
-    	FARMER,
-    	LUMBERJACK,
-    	MINER,
-    	BUILDER,
-    };
+		public enum UnitJob {
+			NOTHING,
+			FARMER,
+			LUMBERJACK,
+			MINER,
+			BUILDER,
+		};
 
-    public enum UnitAction {
-    	WANDERING,
-    	WORKING,
-    	LEARNING,
-    };
+		public enum UnitAction {
+			WANDERING,
+			WORKING,
+			LEARNING,
+		};
 
-    public static class ActionBuilder
-    {
-    	public static Func<Unit, float, int?> ScoreAddByDay(double saturationByDay = 0, double energyByDay = 0, double happynessByDay = 0)
-    	{
-    		return (Unit unit, float delta) =>
-    		{
-                unit.saturationScore = saturationByDay;
-                unit.happynessScore = happynessByDay;
-                unit.energyScore = energyByDay;
-    			return 0;
-    		};
-    	}
-    };
+		public static class ActionBuilder
+		{
+			public static Func<Unit, float, int?> ScoreAddByDay(double saturationByDay = 0, double energyByDay = 0, double happynessByDay = 0)
+			{
+				return (Unit unit, float delta) =>
+				{
+								unit.saturationScore = saturationByDay;
+								unit.happynessScore = happynessByDay;
+								unit.energyScore = energyByDay;
+					return 0;
+				};
+			}
+		};
 
-    //public Dictionary<UnitAction, Func<Unit, float, int?>> ActionDoingDictionnary = new()
-    //{
-    	
-    //	{UnitAction.WANDERING, ActionBuilder.ScoreAddByDay(saturationByDay: -.2)},
-    //	{UnitAction.WORKING, ActionBuilder.ScoreAddByDay(saturationByDay: -.2)},
-    //	{UnitAction.LEARNING, ActionBuilder.ScoreAddByDay(saturationByDay: -.2)},
-    //};
 
-    public class Unit : MonoBehaviour
-    {
-    	public double saturationScore = 1;
-    	public double energyScore = 1;
-    	public double happynessScore = .5;
-    	public UnitAction actualAction = UnitAction.WANDERING;
-    	public UnitJob actualJob = UnitJob.NOTHING;
-    		
-    	// Start is called once before the first execution of Update after the MonoBehaviour is created
-    	void Start()
-    	{
-    		
-    	}
+		public class Unit : MonoBehaviour
+		{
+			public static Dictionary<UnitAction, Func<Unit, float, int?>> ActionDoingDictionnary = new()
+			{
+				{UnitAction.WANDERING, ActionBuilder.ScoreAddByDay(saturationByDay: -.2)},
+				{UnitAction.WORKING, ActionBuilder.ScoreAddByDay(saturationByDay: -.2)},
+				{UnitAction.LEARNING, ActionBuilder.ScoreAddByDay(saturationByDay: -.2)},
+			};
 
-    	// Update is called once per frame
-    	void Update()
-    	{
-    		//this.actualTask.UpdateRepercution(this, Time.deltaTime);
-    	}
-    }
+			public double saturationScore = 1;
+			public double energyScore = 1;
+			public double happynessScore = .5;
+			public UnitAction actualAction = UnitAction.WANDERING;
+			public UnitJob actualJob = UnitJob.NOTHING;
+				
+			// Start is called once before the first execution of Update after the MonoBehaviour is created
+			void Start()
+			{
+				
+			}
+
+			// Update is called once per frame
+			void Update()
+			{
+				ActionDoingDictionnary[this.actualAction](this, Time.deltaTime);
+			}
+		}
 }
