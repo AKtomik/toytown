f73dab91dc2ff741b3fc0f11c372f71b741b2be7
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-12-08 11:42:54 +0100

    GetNearestPlace in gamemana

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -13,4 +16,44 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
+    public enum Place
+	{
+		BUSH,
+        WOOD,
+        MINE,
+        SCHOOL,
+        CANTINE,
+        HOUSE,
+	}
+    
+    public Dictionary<Place, List<Vector3>> PlacePlacing = new()
+	{
+		{Place.BUSH, new()},
+        {Place.WOOD, new()},
+        {Place.MINE, new()},
+        {Place.SCHOOL, new()},
+        {Place.CANTINE, new()},
+        {Place.HOUSE, new()},
+	};
+
+    public Vector3 GetNearestPlace(Place place, Vector3 pos)
+	{
+        if (PlacePlacing[place].Count == 0)
+		{
+			throw new Exception($"PlacePlacing list for {place} is empty but GetNearestPlace is called.");
+		}
+        Vector3 nearestPos = PlacePlacing[place][0];
+        float nearestDistance = Vector3.Distance(PlacePlacing[place][0], pos);
+        foreach (Vector3 placePos in PlacePlacing[place])
+		{
+			float distance = Vector3.Distance(placePos, pos);
+            if (distance < nearestDistance)
+			{
+				nearestDistance = distance;
+                nearestPos = placePos;
+			}
+		}
+		return nearestPos;
+	}
 }
