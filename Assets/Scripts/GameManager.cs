5b8ac8f299f5fa5899a0020769ecb670d564d5b9
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-12-08 12:02:02 +0100

    place selection in gameman

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,26 +16,16 @@ namespace ToyTown {
 
 	public class GameManager : MonoBehaviour
 	{
-		// Start is called once before the first execution of Update after the MonoBehaviour is created
-		void Start()
-		{
-			
-		}
-
-		// Update is called once per frame
-		void Update()
-		{
-			
-		}
 
 		[System.Serializable]
 		public struct PlacePositionList
 		{
 			public Place place;
-			public List<Vector3> positions;
+			public GameObject gameObject;
 		}
+		public List<PlacePositionList> PlaceEditor = new();
 		
-		public Dictionary<Place, List<Vector3>> PlacePlacing = new()
+		public Dictionary<Place, List<GameObject>> PlaceDictionary = new()
 		{
 			{Place.BUSH, new()},
 			{Place.WOOD, new()},
@@ -45,16 +35,32 @@ namespace ToyTown {
 			{Place.HOUSE, new()},
 		};
 
+		// Start is called once before the first execution of Update after the MonoBehaviour is created
+		void Start()
+		{
+			foreach (var item in PlaceEditor)
+			{
+				PlaceDictionary[item.place].Add(item.gameObject);
+			}
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			
+		}
+
 		public Vector3 GetNearestPlace(Place place, Vector3 pos)
 		{
-			if (PlacePlacing[place].Count == 0)
+			if (PlaceDictionary[place].Count == 0)
 			{
-				throw new Exception($"PlacePlacing list for {place} is empty but GetNearestPlace is called.");
+				throw new Exception($"PlaceDictionary list for {place} is empty but GetNearestPlace is called.");
 			}
-			Vector3 nearestPos = PlacePlacing[place][0];
-			float nearestDistance = Vector3.Distance(PlacePlacing[place][0], pos);
-			foreach (Vector3 placePos in PlacePlacing[place])
+			Vector3 nearestPos = PlaceDictionary[place][0].transform.position;
+			float nearestDistance = Vector3.Distance(PlaceDictionary[place][0].transform.position, pos);
+			foreach (GameObject placeObject in PlaceDictionary[place])
 			{
+				Vector3 placePos = placeObject.transform.position;
 				float distance = Vector3.Distance(placePos, pos);
 				if (distance < nearestDistance)
 				{
