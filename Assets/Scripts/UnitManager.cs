a9a1e6485f2e990594712280dbce2c1496a1be87
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-12-12 11:28:00 +0100

    spawning of unit

--- Assets/Scripts/UnitManager.cs
+++ Assets/Scripts/UnitManager.cs
@@ -1,20 +1,38 @@
+using System;
+using UnityEditor.PackageManager;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace ToyTown {
 	public class UnitManager : MonoBehaviour
 	{
 		public static UnitManager Instance { get; private set; }
+		[SerializeField]
+		private GameObject unitPrefab;
+		private double spawnProgress = 1;
 
 		// Start is called once before the first execution of Update after the MonoBehaviour is created
 		void Start()
 		{
 			Instance = this;
+			if (unitPrefab == null) throw new Exception($"unitPrefab is not defined, assign it in the unity editor in UnitManager!");
+			Instantiate(unitPrefab, transform.position, transform.rotation);
 		}
 
 		// Update is called once per frame
 		void Update()
 		{
-			
+			double rand = Random.value * 2 - 1;
+			double variation = 1 + rand * Random.value;
+			if (variation < 0) variation = 1 / Math.Abs(variation);
+			spawnProgress += Time.deltaTime * Settings.SpeedUp / Settings.DayLengthInSecond * variation;
+			if (spawnProgress > 1)
+			{
+				spawnProgress -= 1;
+				Debug.Log($"spawning a new unit at UnitManager");
+				if (unitPrefab == null) throw new Exception($"unitPrefab is not defined, assign it in the unity editor in UnitManager!");
+				Instantiate(unitPrefab, transform.position, transform.rotation);
+			}
 		}
 
 		public Unit[] UnitArray()
