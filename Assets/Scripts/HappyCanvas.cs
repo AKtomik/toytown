badf7375345aea075077e090e2383ed481e11bc6
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-12-12 14:41:00 +0100

    day amount

--- Assets/Scripts/HappyCanvas.cs
+++ Assets/Scripts/HappyCanvas.cs
@@ -3,20 +3,25 @@ using UnityEngine;
 
 namespace ToyTown {
 	public class HappyCanvas : MonoBehaviour
+	{
+		[SerializeField]
+		GameObject dayCounterObject;
+		TextMesh dayCounterMesh;
+
+		// Start is called once before the first execution of Update after the MonoBehaviour is created
+		void Start()
 		{
-			[SerializeField]
-			TextMeshPro dayCounter;
+			//Debug.Log($"{typeof(dayCounterObject)} {GetComponents(dayCounterObject)}");
+			if (!dayCounterObject.TryGetComponent(out dayCounterMesh)) Debug.LogError($"no TextMeshPro component! {dayCounterObject}");;
+		}
 
-			// Start is called once before the first execution of Update after the MonoBehaviour is created
-			void Start()
+		// Update is called once per frame
+		void Update()
+		{
+			if (dayCounterMesh != null)
 			{
-				
-			}
-
-			// Update is called once per frame
-			void Update()
-			{
-				dayCounter.text = $"Day {SunManager.Instance.DayAmount}";
+				dayCounterMesh.text = $"Day {SunManager.Instance.DayAmount}";
 			}
 		}
+	}
 }
