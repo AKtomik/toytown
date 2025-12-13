6d49cf72b980314a8599a276b05936b126f45d69
AKtomik <97125853+AKtomik@users.noreply.github.com>
2025-12-13 20:02:20 +0100

    no more builder enter exit and more

--- Assets/Scripts/Map/Tile.cs
+++ Assets/Scripts/Map/Tile.cs
@@ -3,39 +3,5 @@ using ToyTown;
 
 public class Tile : MonoBehaviour
 {
-    public bool IsBuilderPresent { get; private set; } = false;
-
-    private int builderCount = 0;
-
-    private void OnTriggerEnter(Collider other)
-    {
-        Unit unit = other.GetComponent<Unit>();
-
-        if (unit != null && unit.GetActualJob() == UnitJob.BUILDER)
-        {
-            builderCount++;
-            if (builderCount > 0 && !IsBuilderPresent)
-            {
-                IsBuilderPresent = true;
-                Debug.Log($"Tile {name}: BUILDER entré. Construction possible.");
-            }
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        Unit unit = other.GetComponent<Unit>();
-
-        if (unit != null && unit.GetActualJob() == UnitJob.BUILDER)
-        {
-            builderCount--;
-
-            if (builderCount <= 0 && IsBuilderPresent)
-            {
-                builderCount = 0;
-                IsBuilderPresent = false;
-                Debug.Log($"Tile {name}: Dernier BUILDER parti. Construction en pause.");
-            }
-        }
-    }
+    
 }
